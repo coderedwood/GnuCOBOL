2255c3f11b4a149a4f4fde1d1e5a39438a3701fc
Dwayne Redwood <87104481+coderedwood@users.noreply.github.com>
2023-02-04 19:31:16 -0500

    Added COBOL source for HelloWorld

---
+++ HelloWorld.cbl
@@ -0,0 +1,15 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TEST.
+       
+
+
+       ENVIRONMENT DIVISION.
+       
+       DATA DIVISION.
+       
+       PROCEDURE DIVISION.
+       0100-START-HERE.
+           DISPLAY "Hello World".
+       STOP RUN.
+       END PROGRAM TEST.
+       
